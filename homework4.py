b626785c7ee88f56c58be4d9fc482c97c9109a88
Evgenijnew1 <Evgenij-bogomazov@mail.ru>
2024-06-16 18:46:27 +0300

    Delete homework4.py

--- homework4.py
+++
@@ -1,7 +0,0 @@
-my_string = input("как зовут страждущего: ")
-print(len(my_string))
-print(my_string.upper())
-print(my_string.upper().lower())
-print(my_string[0])
-print(my_string[-1])
-print(my_string.replace(' ', '' ))
